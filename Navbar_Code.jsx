478fea7a409feceb5e2f0015748b210bd5cc63a1
sahadathossine71-png <sahadathossine71@gmail.com>
2025-12-24 21:08:12 +0600

    Add Navbar component with navigation links
    This is a Navbar Code of our SaaS project.

---
+++ Navbar_Code.jsx
@@ -0,0 +1,16 @@
+
+    <nav>
+        <div class="container">
+            <h1>logo</h1>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Tools</li>
+                    <li>About</li>
+                    <li>Contact us</li>
+                </ul>
+            </div>
+           
+            <button>upgrade</button>
+        </div>
+    </nav>
